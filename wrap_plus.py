2d4d9b699ec2016005fa812021369ce9ec020574
evandrocoan <evandrocoan@hotmail.com>
2018-12-20 20:27:28 -0200

    Set the wrap_plus.py debug level to 1 and disabled the file logger

--- wrap_plus.py
+++ wrap_plus.py
@@ -26,9 +26,9 @@ def is_quoted_string(scope_region, scope_name):
 
 
 time_start = 0
-debug_enabled = 127
-log = getLogger(debug_enabled, "wrap_plus", "wrapplus.txt", mode='w')
-# log = getLogger(debug_enabled, "wrap_plus")
+debug_enabled = 1
+# log = getLogger(debug_enabled, "wrap_plus", "wrapplus.txt", mode='w')
+log = getLogger(debug_enabled, "wrap_plus")
 
 
 def plugin_unloaded():
