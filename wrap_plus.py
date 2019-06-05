a0066699850f9732d7a0a9a6a3f37c937863b602
evandrocoan <evandrocoan@hotmail.com>
2019-06-05 02:18:14 -0300

    Added support to latex math environment $$var$$

--- wrap_plus.py
+++ wrap_plus.py
@@ -256,7 +256,7 @@ def CONCAT(*args):
 
 
 first_group = lambda x: r"(?:^[\t \{\}\n]%s(?:````?.*)?)" % x
-blank_line_pattern = re.compile( r'({}$|{}(?:[%/].*)?$|(?:.*"""\\?$))'.format( first_group('*'), first_group('+') ) )
+blank_line_pattern = re.compile( r'({}$|{}(?:[%/].*)?$|(?:.*"""\\?$)|.*\$\$.+)'.format( first_group('*'), first_group('+') ) )
 # print('pattern', blank_line_pattern.pattern)
 
 next_word_pattern = re.compile(r'\s+[^ ]+', re.MULTILINE)
